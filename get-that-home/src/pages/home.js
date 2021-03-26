3e6f58c534c6fcf20401cab4bd38f95d24e97c11
sebastian4260 <br7cervantes@gmail.com>
2021-03-26 03:22:11 -0500

    Create login button component

--- get-that-home/src/pages/home.js
+++ get-that-home/src/pages/home.js
@@ -5,17 +5,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect, useHistory } from "react-router";
 import { Link } from "react-router-dom";
 import PropertyCard from "../components/contents/PropertyCard"
+import StyledLoginButton from "../components/UI/buttons/StyledLoginButton"
 
 function Home (){
   
 
   return(
     
-          
-            <PropertyCard 
-              
-          />
-          
+          <div>
+            <PropertyCard />
+
+          <StyledLoginButton>Login</StyledLoginButton>
+          </div>
           )
   };
        
