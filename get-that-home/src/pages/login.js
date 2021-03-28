073c6d4e3d181a0d71036b8b218a4ee9a6ddcf6b
sebastian4260 <br7cervantes@gmail.com>
2021-03-28 03:06:33 -0500

    Add template to login

---
+++ get-that-home/src/pages/login.js
@@ -0,0 +1,27 @@
+/** @jsxImportSource @emotion/react */
+//import { css } from "@emotion/react";
+import Template from "../pages/template";
+
+
+
+
+function Login (){
+  
+
+  return(
+      <Template>
+
+          
+            
+          
+
+      </Template>
+
+      
+ 
+
+          )
+  };
+       
+
+export default Login;
