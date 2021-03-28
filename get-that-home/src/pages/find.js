d516d508596e47c4ebc59ea52fe5b2891b99b091
sebastian4260 <br7cervantes@gmail.com>
2021-03-28 03:09:07 -0500

    Create find and template pages

---
+++ get-that-home/src/pages/find.js
@@ -0,0 +1,27 @@
+/** @jsxImportSource @emotion/react */
+//import { css } from "@emotion/react";
+import Template from "../pages/template";
+
+
+
+
+function Find (){
+  
+
+  return(
+      <Template>
+
+          
+            
+          
+
+      </Template>
+
+      
+ 
+
+          )
+  };
+       
+
+export default Find;
