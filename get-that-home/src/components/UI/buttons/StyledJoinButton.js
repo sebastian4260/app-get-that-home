debfefead23762d8faac314f8c73f9a7ebf24a07
sebastian4260 <br7cervantes@gmail.com>
2021-03-28 03:08:01 -0500

    Create buttons and styles

---
+++ get-that-home/src/components/UI/buttons/StyledJoinButton.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+const StyledJoinButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  width: auto;
+  height: auto;
+  align-items: center;
+  padding: 8px 16px;
+  border: 1px solid #F48FB1;
+  border-radius: 16px;
+  color: #616161;
+  margin: 0 auto;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 1.25px;
+  text-transform: uppercase;
+`;
+
+export default StyledJoinButton;
