2c7cab1271f9f31c7334f93ab21c281b9adc3214
sebastian4260 <br7cervantes@gmail.com>
2021-04-04 23:22:04 -0500

    Add route to button

--- get-that-home/src/components/contents/StyledCreateAccountSection.js
+++ get-that-home/src/components/contents/StyledCreateAccountSection.js
@@ -2,6 +2,7 @@
 //import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import StyledLoginButton from "../UI/buttons/StyledLoginButton";
+import { Link, useLocation } from "react-router-dom";
 
 const StyledDiv = styled.div`
   position: absolute;
@@ -38,18 +39,60 @@ color: #373737;
 
 `;
 
+const ListItem = styled.li`
+  & > a {
+    position: relative;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+  &.selected {
+            
+            filter: drop-shadow(0px 6px 20px rgba(215, 56, 0, 0.4));
+          }
+  }
+`;
+
+const sections = ["signup"];
+const buttons = {
+  
+  signup: <StyledLoginButton className="ri-signup-line">Create an account now</StyledLoginButton>,
+  
+};
+
+export default function StyledCreateAccountSection() {
+  const location = useLocation();
+  const isSelected = (section) => {
+    return (
+      (location.pathname === "/" && section === "home") ||
+      location.pathname === `/${section}`
+    );
+  };
 
 
-function StyledCreateAccountSection (){
-  return(
+  return (
+
+
     <StyledDiv>
-      <DivTitle>
-        <Title>Getting someone to rent your apartment has never been this easy</Title>
-        <StyledLoginButton>Create an account now</StyledLoginButton>
-      </DivTitle>
 
-    </StyledDiv>
+      <DivTitle>  
+        <Title>Getting someone to rent your apartment has never been this easy</Title>
+
+        {sections.map((section) => (
+        <ListItem key={section}>
+          <Link
+            className={isSelected(section) ? "selected" : ""}
+            to={section === "home" ? "/" : `/${section}`}
+          >
+            {buttons[section]}
+          </Link>
+        </ListItem>
+      ))}
+        
+      </DivTitle>
+      
+      
+      </StyledDiv>
+
   );
 }
 
-export default StyledCreateAccountSection;
